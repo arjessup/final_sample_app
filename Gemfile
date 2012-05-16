56a3a0b94ab33375a07ce520f2cc0eeecd9188b8
Andrea Jessup <jessup.andrea@gmail.com>
2012-05-15 23:54:43 -0500

    Finishd user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -7,10 +7,12 @@ gem 'rails', '3.0.12'
 
 gem 'sqlite3', '1.3.3'
 gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'will_paginate', '3.0.pre2'
 
 
 group :development do
   gem 'annotate', '2.4.0'
+  gem 'faker', '0.3.1'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 FinalSampleApp::Application.routes.draw do
 
+  resources :sessions, :only => [:new, :create, :destroy]
   resources :users
   
-  match 'signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+  match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
